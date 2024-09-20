4bf6241f629afa7eb56ae16ce9c5939ac104ab48
Clifford <clifford.palmer@menloschool.org>
2024-09-19 23:21:33 -0700

    2.5 hrs+ spent

--- src/HighwaysAndHospitals.java
+++ src/HighwaysAndHospitals.java
@@ -4,7 +4,7 @@
  * for Adventures in Algorithms
  * at Menlo School in Atherton, CA
  *
- * Completed by: [YOUR NAME HERE]
+ * Completed by: Clifford Palmer
  *
  */
 import java.util.ArrayList;
@@ -12,49 +12,74 @@ import java.util.Queue;
 import java.util.LinkedList;
 public class HighwaysAndHospitals {
 
-    /**
-     * TODO: Complete this function, cost(), to return the minimum cost to provide
-     *  hospital access for all citizens in Menlo County.
-     */
     public static long cost(int n, int hospitalCost, int highwayCost, int cities[][]) {
+        // If the highway cost is greater than or equal to the hospital cost
         if(highwayCost >= hospitalCost){
+            // Simply directly assign each city a hospital and calculate the cost
             return (long)(n) * (hospitalCost);
         }
 
-        int[] roots = new int[n + 1];
+        /*
+        Array which stores both the root of each city and the order of cities who are their own
+        root for the sake of weight balancing. Positive ints stored in the array represent roots
+        of cities. For cities who are their own root, their order is stored as a negative int.
+         */
+        int[] info = new int[n + 1];
 
+        // Loops through the list of edges
         for(int i = 0; i < cities.length; i++){
+            // Store the cities connected by the edge as ints
             int a = cities[i][0];
             int b = cities[i][1];
+            // Temporary variable to stand in place of a or b, which enables path compression later
             int x = a;
-            while(roots[x] > 0){
-                x = roots[x];
+            // Find the shallowest (as smallest depth) root of the first city
+            while(info[x] > 0){
+                x = info[x];
             }
-            while(roots[a] > 0){
-                int temp = roots[a];
-                roots[a] = x;
+            /*
+            Compress the entire path leading from a to its shallowest root by setting the root of
+            each node in this path to x.
+             */
+            while(info[a] > 0){
+                int temp = info[a];
+                info[a] = x;
                 a = temp;
             }
             x = b;
-            while(roots[x] > 0){
-                x = roots[x];
+            // Find the shallowest root of the second city
+            while(info[x] > 0){
+                x = info[x];
             }
-            while(roots[b] > 0){
-                int temp = roots[b];
-                roots[b] = x;
+            /*
+            Compress the entire path leading from b to its shallowest root by setting the root of
+            each node in this path to x.
+             */
+            while(info[b] > 0){
+                int temp = info[b];
+                info[b] = x;
                 b = temp;
             }
             if(a != b){
-                roots[b] = a;
+                if(info[a] <= info[b]){
+                    info[b] += info[a] - 1;
+                    info[a] = b;
+
+                }
+                else{
+                    info[a] += info[b] - 1;
+                    info[b] = a;
+
+                }
             }
         }
-        long clusters = 0;
-        for(int i = 1; i < roots.length; i++){
-            if(roots[i] <= 0){
+        int clusters = 0;
+        for(int i = 1; i < info.length; i++){
+            if(info[i] <= 0){
                 clusters++;
             }
         }
-        return clusters*hospitalCost + (long)(n-clusters)*highwayCost;
+        return (long)clusters*hospitalCost + (long)(n-clusters)*highwayCost;
     }
 }
 
