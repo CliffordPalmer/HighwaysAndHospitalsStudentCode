f253ec624da448ef18fbf41f831b6a86a671508a
Clifford <clifford.palmer@menloschool.org>
2024-09-19 23:35:18 -0700

    Final Submission

--- src/HighwaysAndHospitals.java
+++ src/HighwaysAndHospitals.java
@@ -7,9 +7,7 @@
  * Completed by: Clifford Palmer
  *
  */
-import java.util.ArrayList;
-import java.util.Queue;
-import java.util.LinkedList;
+
 public class HighwaysAndHospitals {
 
     public static long cost(int n, int hospitalCost, int highwayCost, int cities[][]) {
@@ -18,19 +16,17 @@ public class HighwaysAndHospitals {
             // Simply directly assign each city a hospital and calculate the cost
             return (long)(n) * (hospitalCost);
         }
-
         /*
         Array which stores both the root of each city and the order of cities who are their own
         root for the sake of weight balancing. Positive ints stored in the array represent roots
         of cities. For cities who are their own root, their order is stored as a negative int.
          */
         int[] info = new int[n + 1];
-
         // Loops through the list of edges
-        for(int i = 0; i < cities.length; i++){
+        for(int[] edge : cities){
             // Store the cities connected by the edge as ints
-            int a = cities[i][0];
-            int b = cities[i][1];
+            int a = edge[0];
+            int b = edge[1];
             // Temporary variable to stand in place of a or b, which enables path compression later
             int x = a;
             // Find the shallowest (as smallest depth) root of the first city
@@ -46,6 +42,7 @@ public class HighwaysAndHospitals {
                 info[a] = x;
                 a = temp;
             }
+            // Redefine x to stand in place of b
             x = b;
             // Find the shallowest root of the second city
             while(info[x] > 0){
@@ -60,12 +57,23 @@ public class HighwaysAndHospitals {
                 info[b] = x;
                 b = temp;
             }
+            /*
+            Now, if the shallowest root of a and b aren't the same, meaning that they aren't
+            already part of the same subgraph, make one the root of the other with weight
+            balancing in mind
+             */
             if(a != b){
+                /*
+                 If the order of a is greater than the order of b (but these are stored as
+                 negative ints, so it's actually less than), make a the root of b, and update the
+                 order of a.
+                 */
                 if(info[a] <= info[b]){
                     info[b] += info[a] - 1;
                     info[a] = b;
 
                 }
+                // Otherwise, make b the root of a, and update the order of b.
                 else{
                     info[a] += info[b] - 1;
                     info[b] = a;
@@ -73,12 +81,23 @@ public class HighwaysAndHospitals {
                 }
             }
         }
+        // int to keep track of the amount of disconnected graphs
         int clusters = 0;
+        // Loops through the information about each city
         for(int i = 1; i < info.length; i++){
+            /*
+             If a node doesn't have an actual root, it much be the start to a new subgraph, so
+             add one to clusters.
+             */
             if(info[i] <= 0){
                 clusters++;
             }
         }
+        /*
+        The most optimal money-spending strategy (assuming hospitals are more expensive than
+        highways) is to give each disconnected graph a single hospital, and give each city in
+        those graphs a single road. Do the calculation and return it.
+         */
         return (long)clusters*hospitalCost + (long)(n-clusters)*highwayCost;
     }
 }
